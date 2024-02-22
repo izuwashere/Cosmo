4997b86945162899cb7bdfcfffedbb6ccd166efc
izuwashere <emanuelduque096@gmail.com>
2024-02-22 14:21:52 -0500

    hola

---
+++ life_seed/src/main/java/com/example/life_seed/LifeSeedApplication.java
@@ -0,0 +1,13 @@
+package com.example.life_seed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LifeSeedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LifeSeedApplication.class, args);
+	}
+
+}
---
+++ life_seed/src/test/java/com/example/life_seed/LifeSeedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.life_seed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LifeSeedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
